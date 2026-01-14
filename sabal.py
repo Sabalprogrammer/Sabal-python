ad1c8930841af3138af50ef6a3c0bbee31a91978
sabalprogrammer <sabalbhattarai654@gmail.com>
2026-01-14 23:02:32 +0545

    till operator

--- sabal.py
+++ sabal.py
@@ -1,2 +1,18 @@
 # knowing the basics of 
 print("hello world")
+number =10.0#float
+print("Number is :",number)
+first_name= "sabal"
+last_name= "Bhattarai"
+print(type(first_name))
+print(type(last_name))
+number=(10)
+print(number)
+print(type(number))
+number1=str(10)
+print(number1)
+print(type(number1))
+message="Hello this is me Sabal Bhattarai"
+Hello=message[2:5]
+print(Hello)
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+print(type(last_name))

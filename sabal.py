aacc9671d2dc59ae818411c719b5b231853a6078
sabalprogrammer <sabalbhattarai654@gmail.com>
2026-01-15 12:20:07 +0545

    tabulation

--- sabal.py
+++ sabal.py
@@ -1,18 +1,36 @@
-# knowing the basics of 
-print("hello world")
-number =10.0#float
-print("Number is :",number)
-first_name= "sabal"
-last_name= "Bhattarai"
-print(type(first_name))
-print(type(last_name))
-number=(10)
-print(number)
-print(type(number))
-number1=str(10)
-print(number1)
-print(type(number1))
-message="Hello this is me Sabal Bhattarai"
-Hello=message[2:5]
-print(Hello)
+# # knowing the basics of 
+# print("hello world")
+# number =10.0#float
+# print("Number is :",number)
+# first_name= "sabal"
+# last_name= "Bhattarai"
+# print(type(first_name))
+# print(type(last_name))
+# number=(10)
+# print(number)
+# print(type(number))
+# number1=str(10)
+# print(number1)
+# print(type(number1))
+# message="Hello this is me Sabal Bhattarai"
+# Hello=message[2:5]
+# print(Hello)
+# a="31.2"
+# print(type(a))
+# b=float(a)
+# print(type(b))
 
+# def sum(s1,s2):
+#     return s1+s2
+
+# add=sum(10,12)
+# print(add)
+
+
+import pandas as pd
+data={'Name':["sabal","Ram","shyam"],
+        'age':["10","12","21"],
+        'Gender':["Male","Male","Male"]
+}
+df=pd.DataFrame(data)
+print(df)
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-print(type(last_name))
